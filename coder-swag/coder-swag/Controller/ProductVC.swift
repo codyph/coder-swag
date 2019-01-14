e3168ee2bf5d12b0d510ebe5f31d3430b941a6ef
CodyP <codyphilipp@gmail.com>
2019-01-14 15:14:46 +0800

    Final changes

---
+++ coder-swag/coder-swag/Controller/ProductVC.swift
@@ -0,0 +1,45 @@
+//
+//  ProductVC.swift
+//  coder-swag
+//
+//  Created by Bailey Aldridge on 14/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ProductVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // Outlets
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        } else {
+            return ProductCell()
+        }
+    }
+
+
+}
