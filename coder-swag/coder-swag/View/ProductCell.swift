9b925b96cf3db9eee7213959d9f9ef6ed9b46eef
CodyP <codyphilipp@gmail.com>
2019-01-14 14:35:07 +0800

    Making collection cells with UI

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Bailey Aldridge on 14/1/19.
+//  Copyright © 2019 Cody Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    // Outlets
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
